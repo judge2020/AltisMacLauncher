bf47a2968ab1c8d825f837183ea6cb3666251aff
judge2020 <hunterray55@hotmail.com>
2017-03-04 06:12:02 -0800

    Initial Commit

---
+++ Project Altis/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Project Altis
+//
+//  Created by Hunter Ray on 3/4/17.
+//  Copyright © 2017 Judge2020. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
